a23cbf2895ad66654e96231cdc528d8b13766d62
IMCH222 <chukwuim@mail.uc.edu>
2023-06-04 12:08:52 -0400

    Finished Task 2

--- src/MonthlyCCBalance.java
+++ src/MonthlyCCBalance.java
@@ -1,5 +1,17 @@
-public class MonthlyCCBalance {
+public class MonthlyCCBalance
+{
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    double accBalance = 5000;
+    final double INTEREST_RATE = 0.17;
+    double monthOne = 0;
+    double monthTwo = 0;
+    monthOne = accBalance * INTEREST_RATE;
+        System.out.println("The interest due after one month is:" + " " + "$" + monthOne);
+    accBalance = accBalance + monthOne;
+    monthTwo = accBalance * INTEREST_RATE;
+        System.out.println("The interest due after two months is:" + " " + "$" + monthTwo);
+
+
+
     }
 }
